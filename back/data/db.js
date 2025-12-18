323498bebd59813506217f2b00a9ccb3c54f5261
a23edstorcev <a23edstorcev@inspedralbes.cat>
2025-12-18 08:30:57 +0100

    cambio db

--- back/data/db.js
+++ back/data/db.js
@@ -3,22 +3,11 @@ import "dotenv/config";
 
 const { Pool } = pkg;
 
-// Usamos la DATABASE_URL que definimos en el docker-compose
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
-// Eventos para monitorizar la conexión
-pool.on("connect", () => {
-  console.log("✅ Conexión establecida con PostgreSQL");
-});
+pool.on("connect", () => console.log("✅ Postgres Conectado"));
+pool.on("error", (err) => console.error("❌ Error en el Pool", err));
 
-pool.on("error", (err) => {
-  console.error("❌ Error inesperado en el pool de Postgres", err);
-  // En desarrollo, no queremos que el contenedor se muera siempre,
-  // pero en producción es vital saber que la DB falló.
-});
-
-// Exportación moderna (ES Modules)
-export const query = (text, params) => pool.query(text, params);
 export default pool;
